69348d07061516c12ef68a9f3f267f7268d31ade
Ziggleton <ziggy@devhipsters.com>
2022-02-04 05:22:38 +0200

    migrated section for issues  from previous vite application

---
+++ src/components/IssueList/IssueListComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="issue-list">
+    <p class="issues-title">{{ state.title }}</p>
+    <div class="issue-list-container">
+      <issue-list-item-component
+        v-for="issue in state.list"
+        :key="issue.id"
+        :issue="issue"
+        >{{ issue }}</issue-list-item-component
+      >
+    </div>
+  </section>
+</template>
+
+<script>
+  import { reactive, computed } from "vue";
+  import IssueListItemComponent from "./IssueListItemComponent.vue";
+  export default {
+    props: ["listData"],
+    components: {
+      IssueListItemComponent,
+    },
+    setup(props) {
+      let state = reactive({
+        title: computed(() => {
+          return props.listData.title;
+        }),
+        list: computed(() => {
+          return props.listData.issueList;
+        }),
+      });
+      return {
+        state,
+      };
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .issue-list {
+    margin-top: 1rem;
+    .issues-title {
+      color: $text-muted;
+      margin-bottom: 1rem;
+      font-weight: 500;
+    }
+    .issue-list-container {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
+      grid-gap: 1rem;
+    }
+  }
+</style>
---
+++ src/components/IssueList/IssueListItemComponent.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="list-item" @click="state.changeEmoji()">
+    <div class="emoji">
+      <span :ref="state.emojiItem">{{ state.renderEmoji }}</span>
+    </div>
+    <div class="content">
+      <h4 class="item-title">{{ state.issue.title }}</h4>
+      <p v-if="state.issue.milestone">
+        Milestone: {{ state.issue.milestone.title }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { reactive, computed, ref } from "vue";
+  import { useStore } from "vuex";
+  export default {
+    props: ["issue"],
+    setup(props) {
+      const store = useStore();
+      let state = reactive({
+        emojiItem: computed(() => {
+          return ref();
+        }),
+        setEmojis: computed(() => {
+          return store.state.issues.setEmojis();
+        }),
+        emojiList: computed(() => {
+          return store.state.issues.emojis;
+        }),
+        issue: computed(() => {
+          return props.issue;
+        }),
+        randomEmoji: computed(() => {
+          return state.emojiList[
+            Math.floor(Math.random() * state.emojiList.length)
+          ];
+        }),
+        changeEmoji: async () => {
+          state.emojiItem.value.textContent =
+            store.state.issues.emojis[
+              Math.floor(Math.random() * store.state.issues.emojis.length)
+            ].emoji;
+          store.dispatch("updateIssue", {
+            id: state.issue.id,
+            emoji: state.emojiItem.value.textContent,
+          });
+        },
+        renderEmoji: computed(() => {
+          if (!state.setEmojis)
+            return (
+              state?.emojiItem?.value?.textContent || state.randomEmoji.emoji
+            );
+          const item = state.setEmojis.find((i) => i.id == state.issue.id);
+          if (item) {
+            return item.emoji;
+          } else {
+            return state.randomEmoji.emoji;
+          }
+        }),
+      });
+
+      return { state };
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .list-item {
+    background: $brand-primary;
+    color: $white;
+    display: flex;
+    flex-direction: row;
+    border-radius: 10px;
+    min-height: 90px;
+    overflow: hidden;
+    .emoji {
+      width: 46px;
+      background: $black;
+      display: flex;
+      padding: 0px 16px;
+      flex-direction: row;
+      align-items: center;
+    }
+    .content {
+      padding: 8px;
+      flex: 1;
+    }
+    &:hover {
+      cursor: pointer;
+    }
+  }
+</style>
